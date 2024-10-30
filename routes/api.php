02bfe9f4ed4ddc5c7337b2c7ab2567042cd2d3f5
Edito Silva <edito.santos.silva@gmail.com>
2024-10-30 17:06:21 -0300

    feat: configurations docker-file and composer execute and api sanctun laravel 11 and add routes api.php

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('teste-api', function() {
+    return ['message' => 'new API Laravel 11'];
+});
